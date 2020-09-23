eab6dc81caafcac3780831f1f32ec0cf8d86555d
bertyn99 <bboulikou@gmail.com>
2020-09-23 22:19:44 +0200

    add template

--- jsec.js
+++ jsec.js
@@ -7,16 +7,27 @@ function register() {
     students.push({ //rajoute l'eleve 
         firstName: fname,
         lastName: lname,
-        timestamp: time.toString
+        timestamp: time.toString()
     })
     console.log(students[0])
-
+    display(students)
 
 }
 
-function display() {
-    for (student of students) { // affiche les étudiant present
-        //afficher les eleve
+function display(classroom) {
+   let d= document.querySelector('#classroom')
+    d.hidden=false
+    const tbody = document.querySelector("tbody");
+    tbody.removeChild(tbody.childNodes)
+    for (student of classroom) { // affiche les étudiant present
+        const studTemplate = document.querySelector("#eleve"); //recup le template
+        const clone = document.importNode(studTemplate.content, true);
+        let th = clone.querySelector("th");
+        th.textContent=1
+        let td = clone.querySelectorAll("td");
+        td[0].textContent = student.firstName;
+        td[1].textContent = student.lastName;
+        tbody.appendChild(clone);
     }
 }
 
